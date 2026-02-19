11994e3dc5091858c41e2db91dec4df571ee129b
Yash-Anokar <anokaryashraj@gmail.com>
2026-02-19 16:46:59 +0530

    app.py

--- app.py
+++ app.py
@@ -1037,8 +1037,9 @@ def internal_error(error):
 
 # ============= RUN APPLICATION =============
 
+import os
+
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
 
